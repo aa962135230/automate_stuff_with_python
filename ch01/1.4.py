3be56c2b43a0a5e8558f97d378fa17f76cebaddb
kindleeldink <37297312@qq.com>
2020-08-11 19:25:29 +0800

    add ch01

---
+++ ch01/1.4.py
@@ -0,0 +1,17 @@
+# 使用变量:
+# 用于存放一个值,类似于使用一个带标签的盒子盛放东西,通过标签可以找到盒子所装的东西.
+# 赋值格式: 变量名 = 值
+spam = 40
+print("spam is:", spam)
+
+eggs=2
+print("spam + eggs is: ", spam+eggs)
+print("spam + eggs + spam is: ", spam+eggs+spam)
+
+print("进行spam = spam + 2操作后")
+spam = spam + 2
+print("spam is: ",spam)
+
+# 变量名只能是以数字 字母 下划线组合,且数字不能放首位
+# 变量名区分大小写
+
